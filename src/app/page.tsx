5f8c39685d1e1334c9f246a8a405a8dd29146204
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-08-08 19:12:47 +0500

    complete Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import React from "react";
 
 function page() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <div className="text-3xl mt-5 text-center">Habibian Blog</div>
+      <HeroSection />
     </main>
   );
 }
